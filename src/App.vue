30d6325d6c19828118008f1d25b5661fa76af655
李显 <lixian@yuanian.com>
2023-11-07 23:10:49 +0800

    add: curriculum vitae

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Just do it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/curriculumVitae">简历</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/CurriculumVitae.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="outer-wrap">
+    <div class="content-warp"></div>
+  </div>
+</template>
+
+<style scoped>
+.outer-wrap {
+  border: 1px solid #ccc;
+  width: 210mm;
+  height: 100vh;
+  overflow-y: auto;
+}
+.outer-wrap .content-warp {
+  height: 297mm;
+}
+</style>
